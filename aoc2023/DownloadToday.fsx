c9c45c5bfd4eac1f516ca2745b9ec2d346ba5ed9
Alexey Kobiakov <alexey@kobiakov.eu>
2023-12-03 11:29:58 +0100

    Daily fetcher

--- aoc2023/DownloadToday.fsx
+++ aoc2023/DownloadToday.fsx
@@ -1,16 +1,21 @@
 #!/usr/bin/env -S dotnet fsi
 #r "nuget: FsHttp"
 
+open System
 open FsHttp
+open FsHttp.Domain
 
-http {
-    POST "https://reqres.in/api/users"
-    CacheControl "no-cache"
-    body
-    jsonSerialize
-        {|
-            name = "morpheus"
-            job = "leader"
-        |}
-}
-|> Request.send
+type Token = string
+
+let readAoCToken = System.IO.File.ReadAllText ".token" |> String.s
+    
+let fetchInputForGivenDay (token: string) (day: int) = 
+    http {
+        GET $"https://adventofcode.com/2023/day/%d{day}/input"
+        CacheControl "no-cache"
+        header "cookie" $"session=%s{token}" 
+    }
+    |> Request.send
+    |> Response.toText
+
+printf $"$s{fetchInputForGivenDay readAoCToken 1}"
